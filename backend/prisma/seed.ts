61548384978643770b8b3d8df4250b4e84b9427f
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-01-18 04:07:00 -0300

    Criando seed para pre popular o banco de dados

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  await prisma.habit.create({
+    data:{
+        title: "Beber 2L de aguá",
+        created_at: new Date("2023-01-28T00:00:00.000z")
+    }
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })

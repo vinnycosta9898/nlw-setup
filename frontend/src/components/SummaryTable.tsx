5c33e19bee0de1f3aa3b91bcef6902e15f3aee78
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-01-18 20:35:16 -0300

    criando a funcao generate-dates

--- frontend/src/components/SummaryTable.tsx
+++ frontend/src/components/SummaryTable.tsx
@@ -1,4 +1,15 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning-"
+import { HabitDay } from "./HabitDay"
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"]
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSizes = 18 * 7 
+
+const amountOfDaystoFill = minimumSummaryDatesSizes - summaryDates.length
+
+console.log(summaryDates)
 
 export function SummaryTable(){
     return(
@@ -13,8 +24,22 @@ export function SummaryTable(){
                 })}
             </div>
 
-            <div className="grid grid-rows-7 grid-flow-row gap-3">
-                <div className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg"></div>
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {summaryDates.map(date => {
+                    return <HabitDay key={date.toString()}/>
+                })
+                }
+
+                {amountOfDaystoFill > 0 && Array.from({length: amountOfDaystoFill}).map((_, index) => {
+                    return(
+                        <div 
+                            key={index} 
+                            className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-40 cursor-not-allowed">
+                        </div>
+                    )
+                })
+                }
+
             </div>
         </div>
     )

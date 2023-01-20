12d6104c962eab58a799fbc93dd0e438e72bb69b
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-01-20 11:33:56 -0300

    Adicionando condicionais no componente HabitDay

--- frontend/src/components/HabitDay.tsx
+++ frontend/src/components/HabitDay.tsx
@@ -1,5 +1,6 @@
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from './ProgressBar';
+import clsx from 'clsx'
 
 interface HabitDayProps{
     completed: number;
@@ -11,7 +12,20 @@ export function HabitDay( { completed, amount }: HabitDayProps){
     const completedPercentage = Math.round((completed / amount) * 100)
     return(
         <Popover.Root>
-            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg"></Popover.Trigger>
+            <Popover.Trigger 
+                className={clsx("w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg", {
+                "bg-zinc-900 border-zinc-800" : completedPercentage === 0,
+                "bg-violet-900 border-violet-700" : completedPercentage >= 0 && completedPercentage < 20,
+                "bg-violet-800 border-violet-600" : completedPercentage >= 20 && completedPercentage < 40,
+                "bg-violet-700 border-violet-500" : completedPercentage >= 40 && completedPercentage < 60, 
+                "bg-violet-600 border-violet-500" : completedPercentage >= 60 && completedPercentage < 80,
+                "bg-violet-500 border-violet-400" : completedPercentage >= 80, 
+ 
+
+
+            })}>
+            </Popover.Trigger>
+            
             <Popover.Portal>
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col "> 
 

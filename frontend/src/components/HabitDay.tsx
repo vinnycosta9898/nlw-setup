37edb5b284522bad9b884282923bbeae1dc0a302
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-01-21 09:05:53 -0300

    Buscando resumo da API

--- frontend/src/components/HabitDay.tsx
+++ frontend/src/components/HabitDay.tsx
@@ -5,26 +5,26 @@ import clsx from 'clsx'
 import { Check } from 'phosphor-react';
 
 interface HabitDayProps{
-    completed: number;
-    amount: number; 
+    amount?: number;
+    completed?: number;
+    date: Date;
+
 }
 
-export function HabitDay( { completed, amount }: HabitDayProps){
+export function HabitDay( { completed=0, amount=0 }: HabitDayProps){
 
-    const completedPercentage = Math.round((completed / amount) * 100)
+    const completedPercentage = amount > 0  ? Math.round((completed / amount) * 100) : 0
+    
     return(
         <Popover.Root>
             <Popover.Trigger 
-                className={clsx("w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg", {
+                className={clsx("w-10 h-10 border-2 rounded-lg", {
                 "bg-zinc-900 border-zinc-800" : completedPercentage === 0,
-                "bg-violet-900 border-violet-700" : completedPercentage >= 0 && completedPercentage < 20,
+                "bg-violet-900 border-violet-700" : completedPercentage > 0 && completedPercentage < 20,
                 "bg-violet-800 border-violet-600" : completedPercentage >= 20 && completedPercentage < 40,
                 "bg-violet-700 border-violet-500" : completedPercentage >= 40 && completedPercentage < 60, 
                 "bg-violet-600 border-violet-500" : completedPercentage >= 60 && completedPercentage < 80,
                 "bg-violet-500 border-violet-400" : completedPercentage >= 80, 
- 
-
-
             })}>
             </Popover.Trigger>
             

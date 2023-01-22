62c172500d236951eca81046e50250d09881ae09
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-01-22 12:11:15 -0300

    Desabilitando datas passadas

--- frontend/src/components/HabitsList.tsx
+++ frontend/src/components/HabitsList.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import * as CheckBox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import { api } from '../lib/axios';
+import dayjs from 'dayjs';
 
 interface HabitsListProps{
     date: Date
@@ -29,6 +30,9 @@ export function HabitsList({ date } : HabitsListProps){
         })
     }, [])
 
+    const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
+
+
     return(
         <div className="mt-6 flex flex-col gap-3">
             {habitsInfo?.possibleHabits.map(habit => {
@@ -36,6 +40,7 @@ export function HabitsList({ date } : HabitsListProps){
                     <CheckBox.Root
                         key={habit.id}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
+                        disabled={isDateInPast}
                         className="flex items-center gap-3 group"
                     >
                         <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">

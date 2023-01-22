48620b2f011766ef7ff35e064d836c1a8b73e8d7
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-01-22 14:02:49 -0300

    Finalizando Frontend da aplicacao

--- frontend/src/components/NewHabitForm.tsx
+++ frontend/src/components/NewHabitForm.tsx
@@ -57,7 +57,7 @@ export function NewHabitForm(){
                 type="text"
                 id="title"
                 placeholder="Ex: Exercicios, Dormir Bem etc..."
-                className="p-4 round-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:ring-2 focus:outline-none focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
                 autoFocus
                 value={title}
                 onChange={event => setTitle(event.target.value)}
@@ -76,14 +76,14 @@ export function NewHabitForm(){
                     availableWeekDays.map((weekDay, index) => {
                         return(
                             <CheckBox.Root
-                                className="flex items-center gap-3 group"
+                                className="flex items-center gap-3 group focus:outline-none"
                                 key={weekDay}
                                 checked={weekDays.includes(index)}
                                 onCheckedChange={() => {
                                     handleToggleWeekDay(index)
                                 }}
                             >
-                                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors">
+                                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
                                     <CheckBox.Indicator>
                                         <Check size={20} className="text-white"/>
                                     </CheckBox.Indicator>
@@ -101,7 +101,7 @@ export function NewHabitForm(){
 
             <button 
                 type="submit"
-                className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+                className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
             >
                 <Check size={20} weight="bold"/>
                 Confirmar
